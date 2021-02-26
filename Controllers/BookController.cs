251712e5fa5c7a0ef46e8adf967e1b06beddf950
chauhanr <chauhanr@deakin.edu.au>
2021-02-26 19:43:20 +1100

    updated controller

---
+++ Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BookListRazor.Models;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Controllers
+{   [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            
+            return Json(new { data=_db.Book.ToList()});
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,9 @@ namespace BookListRazor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(option=>option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews();
             services.AddRazorPages();
+            
 
         }
 
@@ -53,6 +55,7 @@ namespace BookListRazor
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
